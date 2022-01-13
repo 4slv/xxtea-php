4a1cf64b16e6af9f53d3b2c2a262c854e337863b
4slv <34535446+4slv@users.noreply.github.com>
2022-01-13 21:03:28 +0300

    Update XXTEATest.php

--- tests/XXTEATest.php
+++ tests/XXTEATest.php
@@ -26,7 +26,7 @@ class XXTEATest extends PHPUnit_Framework_TestCase {
         $str = "Hello World!";
         $key = "1234567890";
         $encrypt_data = Xxtea::encrypt_base64($str, $key);
-        $this->assertEquals($encrypt_data, "AkwSi8IEfZxDejCdKQBLWL7w1zGktADVekoAXQ==");
+        $this->assertEquals("AkwSi8IEfZxDejCdKQBLWL7w1zGktADVekoAXQ==", $encrypt_data);
     }
 
     public function testDecryptBase64() {
@@ -35,5 +35,23 @@ class XXTEATest extends PHPUnit_Framework_TestCase {
         $encrypt_data = Xxtea::encrypt_base64($str, $key);
         $decrypt_data = Xxtea::decrypt_base64($encrypt_data, $key);
         $this->assertEquals($decrypt_data, $str);
+    }
+
+    public function testEncryptDataToBase64() {
+        $array = ['a' => "Hello", 'b' => "World!"];
+        $key = "1234567890";
+        $encrypt_data = Xxtea::encrypt_data_to_base64($array, $key);
+        $this->assertEquals(
+            "an0io4tFOuXSo2ht4wVd8zzsgZvaTo5Jq5oU6MGhApOxjLJpN5qNiRmrXM/9Mc8F7h7MdtuqFZZISX33+mmlrw==",
+            $encrypt_data
+        );
+    }
+
+    public function testDecryptDataFromBase64() {
+        $array = ['a' => "Hello", 'b' => "World!"];
+        $key = "1234567890";
+        $encrypt_data = Xxtea::encrypt_data_to_base64($array, $key);
+        $decrypt_data = Xxtea::decrypt_data_from_base64($encrypt_data, $key);
+        $this->assertEquals($array['a'], $decrypt_data['a']);
     }
 }
